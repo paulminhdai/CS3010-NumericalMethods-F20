988c88158c1c7cc1f6b33472560c56c8376e44c4
Dai Vuong <daivuong@Dai-Vuong-2.local>
2020-09-20 22:58:17 -0700

    add function enter equation and read from file

--- Gaussian.java
+++ Gaussian.java
@@ -1,4 +1,8 @@
 import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 public class Gaussian {
 
@@ -7,13 +11,13 @@ public class Gaussian {
 
         // Find the scale vector and store them in array scale.
         double [] scale = new double[n];
-        for(int i=0; i<n; i++) {
+        for(int r=0; r<n; r++) {
             double max = 0;
-            for(int j=0; j<n; j++) {
-                if(Math.abs(m[i][j]) > max)
-                    max = Math.abs(m[i][j]); 
+            for(int c=0; c<n; c++) {
+                if(Math.abs(m[r][c]) > max)
+                    max = Math.abs(m[r][c]); 
             }
-            scale[i] = max;
+            scale[r] = max;
         }
         // Print out the scale vector
         System.out.print("The scale vector: ");
@@ -23,6 +27,7 @@ public class Gaussian {
         
 
         for(int i=0; i<n; i++) {
+
             //print the matrix
             for(int r=0; r<n; r++) {
                 for(int c=0; c<n+1; c++) {
@@ -31,9 +36,10 @@ public class Gaussian {
                 System.out.printf("%n");
             }
 
-            pivot(m, n, i, scale);
-
             
+            pivot(m, n, i, scale); // call pivot funtion to calculate scaled pivot and max
+
+            // Eliminate each row 
             for(int row=i+1; row<n; row++) {
                 double div = m[row][i]/m[i][i];
                 for(int col=0; col<n+1; col++) {
@@ -41,8 +47,26 @@ public class Gaussian {
                 }
             }
         }
-        /*if(m[n-1][n-1] == 0)
-            throw new Exception("There's no unique solution");*/
+
+        // Detect infinite solutions (no unique solution)
+        try {
+            if(m[n-1][n-1] == 0 && m[n-1][n] == 0)
+                throw new Exception("There's no unique solution");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
+        // Detect no solutions
+        try {
+            if(m[n-1][n-1] == 0)
+                throw new Exception("There's no solution");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
 
         // Find x1,x2,x3,... after eliminate.
         double [] result = new double[n];
@@ -55,10 +79,11 @@ public class Gaussian {
         }
 
         // Print out the result
-        System.out.print("The result is: ");
+        System.out.println("The result is: ");
         for(int res=0; res<result.length; res++){
-            System.out.printf("%5.2f", result[res]);
+            System.out.printf("x%d = %5.2f%n",(res+1), result[res]);
         }
+        System.out.printf("%n");
     };
 
     // Pivot function calculate the scaled ratio and swap the rows.
@@ -83,31 +108,100 @@ public class Gaussian {
         System.out.println("row " + rowSelected + " is selected."); // Print the selected row
         System.out.println("\n");
 
-        // Swap rows
+        // Swap the scaled pivot array
         double temp = scale[i];
         scale[i] = scale[maxRow];
         scale[maxRow] = temp;
-        
+        // Swap the row of the matrix
         double [] temp1 = m[i];
         m[i] = m[maxRow];
         m[maxRow] = temp1;
         
     }
     
+    // Function enter info 
+    private static double [][] enterMatrix() {
+        Scanner kb = new Scanner(System.in);
+        int row = 0;
+        try {
+            System.out.println("Enter the number of equations: ");
+            row = kb.nextInt();
+        }
+        catch (Exception e) {
+            System.out.println("Enter the invalid value.");
+        }
+
+        double [][] matrix = new double [row][row+1]; // declare matrix
+        /*
+        try {
+            System.out.println("Enter each equation row by row: ");
+
+            for (int i=0; i<row; i++) {
+                for (int j=0; j<row+1; j++) {
+                    matrix[i][j] = kb.nextInt();
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Enter the invalid value.");
+        }
+        */
+        // import equations from text file
+        try {
+            Scanner reader = new Scanner(new BufferedReader(new FileReader("gaussian.txt")));
+            while(reader.hasNextLine()) {
+                for (int i=0; i<matrix.length; i++) {
+                    String[] line = reader.nextLine().trim().split(" ");
+                    for (int j=0; j<line.length; j++) {
+                        matrix[i][j] = Double.parseDouble(line[j]);
+                    }
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+
+        return matrix;
+    };
+
+
     
-    public static void main(String args []) {
+    public static void main(String args [] ) {
+        
+        double [][]m = enterMatrix(); // Enter the number of equations and input the coefficients. 
+        
         
         // Test cases
-        double [][]m = {{2,3,0,8},{-1,2,-1,0},{3,0,2,9}};
+        /*double [][]m = {{2,3,0,8},
+                          {-1,2,-1,0},
+                          {3,0,2,9}};*/
+
+        // No unique solution
+        /*double [][]m = {{-3,-5,36,10},
+                          {-1,0,7,5},
+                          {1,1,-10,-4}};*/
+        
+        // No solution
+        /*double [][]m = {{1,1,1,2},
+                        {0,1,-3,1},
+                        {2,1,5,0}};*/
+
+
         /*double [][]m = {{3,-13,9,3,-19},
                         {-6,4,1,18,-34},
                         {6,-2,2,4,16},
                         {12,-8,6,10,26}};*/
+
         /*double [][]m = {{1,0,3,0,0},
                         {0,1,3,-1,0},
                         {3,-3,0,6,0},
                         {0,2,4,6,0}};*/
+
         gaussianElimination(m);
-        
+
     }
 }
