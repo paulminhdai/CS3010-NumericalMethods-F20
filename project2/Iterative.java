5b1cf74a4b82f5b3cd42c6f381b587c506dd4bf9
Dai Vuong <daivuong@Dai-Vuong-2.local>
2020-09-25 17:46:28 -0700

    work so far

--- project2/Iterative.java
+++ project2/Iterative.java
@@ -14,7 +14,7 @@ public class Iterative {
 
     
 
-    // Function enter info 
+    // Function enter equations.
     private static double [][] enterMatrix() {
         Scanner kb = new Scanner(System.in);
         int row = 0;
@@ -26,6 +26,7 @@ public class Iterative {
         }
         catch (Exception e) {
             System.out.println("Enter the invalid value.");
+            System.exit(0);
         }
 
         double [][] matrix = new double [row][row+1]; // declare matrix
@@ -33,6 +34,7 @@ public class Iterative {
         int option;
         
         System.out.println("Do you want to enter the equations in the console or enter a file name? Choose:\n\t1 - enter coefficients equations.\n\t2 - enter file name.");
+        System.out.print("Enter: ");
         option = kb.nextInt();
         while (option < 1 || option > 2) {
             System.out.print("Enter 1 or 2 again to choose options: ");
@@ -42,7 +44,7 @@ public class Iterative {
 
         if (option == 1) { //import equations from console
             try {
-                System.out.println("Enter each equation row by row: ");
+                System.out.println("Enter the coefficients row by row ( includes the b value):  ");
 
                 for (int r=0; r<row; r++) {
                     for (int c=0; c<row+1; c++) {
@@ -62,7 +64,12 @@ public class Iterative {
             System.out.print("Enter the file name: ");
             Scanner read = new Scanner("");
             try {
-                read = new Scanner(new File(kb.next()));
+                File file = new File(kb.next());
+                while (!file.exists()) {
+                    System.out.print("Enter the file name again: ");
+                    file = new File(kb.next());
+                }
+                read = new Scanner(file);
                 for(int r = 0; r < row; r++) {
                     for(int c = 0; c < row+1; c++) {
                         matrix[r][c] = read.nextDouble();
@@ -71,7 +78,6 @@ public class Iterative {
             } 
             catch (Exception e) {
                 System.out.println("An error occurred. Either the file is not found or invalid values.");
-                e.printStackTrace();
                 System.exit(0);
             }
             read.close();
@@ -79,12 +85,15 @@ public class Iterative {
         return matrix;
     };
 
+    // Jacobi method
     private static void jacobi(double [][] matrix, double [] start, double err) {
         int REPEAT_MAX = 50;
         int count = 0;
         int n = matrix.length;
         double [] curr = new double[n]; // current iteration k
         double [] prev = (double[])start.clone(); // previous k-1
+
+        System.out.println("\nJacobi method.");
 
         while(true) {
             for (int r=0; r<n; r++) {
@@ -96,11 +105,11 @@ public class Iterative {
                 curr[r] = add/matrix[r][r]; // current iteration for each variable
             }
             // print out current iteration
-            System.out.printf("x%-2d = [", count);
+            System.out.printf("x%-2d = [", count+1);
             for (int i=0; i<n; i++) {
                 System.out.printf("%8.4f  ", curr[i]);
             }
-            System.out.println("]");
+            System.out.println("]T");
 
             count++;
 
@@ -124,10 +133,84 @@ public class Iterative {
         }
     }
 
+    // Gauss-Seidel method
+    private static void gauss_seidel(double [][] matrix, double [] start, double err) {
+        int REPEAT_MAX = 50;
+        int count = 0;
+        int n = matrix.length;
+        double [] curr = new double[n]; // current iteration k
+        double [] prev = (double[])start.clone(); // previous k-1
+
+        System.out.println("Gauss-Seidel method.");
+
+        while(true) {
+            for (int r=0; r<n; r++) {
+                double add = matrix[r][n];
+                for (int c=0; c<n; c++) {
+                    if (c != r) {
+                        if (c<r)
+                            add -= matrix[r][c] * curr[c]; // xj(k1)
+                        else
+                            add -= matrix[r][c] * prev[c]; // xj(k-1)
+                    }
+                }
+                curr[r] = add/matrix[r][r]; // current iteration for each variable
+            }
+            // print out current iteration
+            System.out.printf("x%-2d = [", count+1);
+            for (int i=0; i<n; i++) {
+                System.out.printf("%8.4f  ", curr[i]);
+            }
+            System.out.println("]T");
+
+            count++;
+
+            boolean cont = true;
+
+            // check for error by using norm l_2
+            double numerator = 0;
+            double  denominator = 0;
+
+            for (int i=0; i<n; i++) {
+                numerator += Math.pow((curr[i]-prev[i]), 2);
+                denominator += Math.pow(curr[i], 2);
+            }
+            if ((Math.sqrt(numerator)/Math.sqrt(denominator)) > err)
+                cont = false;
+
+            // Check the current hit the 50 iterations or over the error
+            if (cont && count <= REPEAT_MAX)
+                break;
+            
+            prev = (double[])curr.clone(); // update new current for next iteration
+        }
+    }
+
     public static void main(String args [] ) {
+
+        Scanner kb = new Scanner(System.in);
+
+        double [][] matrix = enterMatrix(); // Enter the number of equations and input the coefficients. 
+        double [] start = new double[matrix.length];
+        double err = 1e-15;
+
+        try {
+            System.out.print("Enter the desired stopping error: ");
+            err = kb.nextDouble();
+            System.out.print("Enter the starting solution: ");
+            for (int i=0; i<start.length; i++)
+                start[i] = kb.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Invalid error value or start points value.");
+        }
         
-        double [][]matrix = enterMatrix(); // Enter the number of equations and input the coefficients. 
-        // Test cases
+        jacobi(matrix, start, err);
+        System.out.println("\n");
+        gauss_seidel(matrix, start, err);
+    }
+}
+
+// Test cases
         /*double [][]m = {{2,3,0,8},
                           {-1,2,-1,0},
                           {3,0,2,9}};*/
@@ -152,7 +235,3 @@ public class Iterative {
                         {0,1,3,-1,0},
                         {3,-3,0,6,0},
                         {0,2,4,6,0}};*/
-        double [] start = {0,0,0,0};
-        jacobi(matrix, start, 1e-15);
-    }
-}
